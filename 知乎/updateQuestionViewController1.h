c00db7ece889e61d4aa14fe89d35e9971c1918c1
徐晓宁 <xuxiaoning@xuxiaoningdeMacBook-Pro.local>
2021-03-30 23:23:15 +0800

    20210330 update question

---
+++ 知乎/updateQuestionViewController1.h
@@ -0,0 +1,16 @@
+//
+//  updateQuestionViewController1.h
+//  知乎
+//
+//  Created by 徐晓宁 on 2021/3/23.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface updateQuestionViewController1 : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 知乎/updateQuestionViewController1.m
@@ -0,0 +1,31 @@
+//
+//  updateQuestionViewController1.m
+//  知乎
+//
+//  Created by 徐晓宁 on 2021/3/23.
+//
+
+#import "updateQuestionViewController1.h"
+
+@interface updateQuestionViewController1 ()
+
+@end
+
+@implementation updateQuestionViewController1
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
